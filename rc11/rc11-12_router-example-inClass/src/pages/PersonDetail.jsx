82f6e129fd1ca5fa65f9ba8c09aa3547a75026cf
Haliboss <halibos@gmail.com>
2022-10-22 13:54:47 +0300

    go back

--- rc11/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ rc11/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+
 
 const PersonDetail = () => {
-  return (
-    <div>PersonDetail</div>
-  )
-}
+  const { id } = useParams();
+  console.log(id);
 
-export default PersonDetail
+  const { state: person } = useLocation();
+
+  const navigate = useNavigate();
+
+  return <div className="container text-center" >
+    <h3>
+        {person.first_name} {person.last_name}
+    </h3>
+    <img className="rounded" src={person.avatar} alt="" />
+    <p>{person.email}</p>
+    <div>
+        <button className="btn btn-success me-2" onClick={()=> navigate('/')} >Home</button>
+        <button className="btn btn-warning" onClick={()=> navigate(-1)} >Go Back</button>
+      </div>
+  </div>;
+};
+
+export default PersonDetail;
