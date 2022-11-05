babb484fd2b8a80f94ecfe684427a743f1ad8060
Haliboss <halibos@gmail.com>
2022-11-05 08:38:07 +0300

    add new lesson

---
+++ rc08-repeat/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import UseEffectHook from "./components/UseEffectHook";
+
+function App() {
+  const [show, setShow] = useState(true);
+
+  return (
+    <div className="container text-center mt-4">
+      <button className="btn btn-dark" onClick={() => setShow(!show)}>
+        {show ? "Hide" : "Show"}
+      </button>
+
+      {/* Short Circuit */}
+      {/* {show && <LifeCycleMethods />} */}
+
+      {/* show && <UseEffectHook /> */}
+    </div>
+  );
+}
+
+export default App;
