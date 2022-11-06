a895c3a614c9a54af679d95474b4c8eb21b63594
Haliboss <halibos@gmail.com>
2022-11-06 03:45:03 +0300

    change

--- rc09_useEffect-axios-example/tutorial-app-inclass/src/components/TutorialList.jsx
+++ rc09_useEffect-axios-example/tutorial-app-inclass/src/components/TutorialList.jsx
@@ -1,36 +1,7 @@
 import { FaEdit } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
-import axios from "axios";
-import EditTutorial from "./EditTutorial";
-import { useState } from "react";
 
-const TutorialList = ({ tutor, getTutorials }) => {
-  const [editItem, setEditItem] = useState("");
-
-  //! DELETE (CRUD-Delete)
-  const deleteTutorial = async (id) => {
-    const url = "https://tutorials-api-cw.herokuapp.com/api/tutorials";
-    try {
-      await axios.delete(`${url}/${id}`);
-    } catch (error) {
-      console.log(error);
-    }
-    getTutorials();
-  };
-
-  // //! PUT (CRUD-Update)
-  // //! PUT: Whole Update, PATCH: Partially Update
-  // const editTutorial = async ({ id, title, description }) => {
-  //   // const { id, title, description } = item;
-  //   const url = "https://tutorials-api-cw.herokuapp.com/api/tutorials";
-  //   try {
-  //     await axios.put(`${url}/${id}`, { title, description });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  //   getTutorials();
-  // };
-
+const TutorialList = () => {
   return (
     <div className="container mt-4">
       <table className="table table-striped">
@@ -45,43 +16,23 @@ const TutorialList = ({ tutor, getTutorials }) => {
           </tr>
         </thead>
         <tbody>
-          {tutor?.map((item) => {
-            const { id, title, description } = item;
-            return (
-              <tr key={id}>
-                <th>{id}</th>
-                <td>{title}</td>
-                <td>{description}</td>
-                <td className="text-center text-nowrap">
-                  <FaEdit
-                    size={20}
-                    type="button"
-                    data-bs-toggle="modal"
-                    data-bs-target="#edit-modal"
-                    className="me-2 text-warning"
-                    // onClick={() =>
-                    //   editTutorial({
-                    //     id: "1581",
-                    //     title: "UPDATE",
-                    //     description: "UPDATE",
-                    //   })
-                    // }
-                    onClick={() => setEditItem(item)}
-                  />
-                  <AiFillDelete
-                    size={22}
-                    type="button"
-                    className="text-danger "
-                    onClick={() => deleteTutorial(id)}
-                  />
-                </td>
-              </tr>
-            );
-          })}
+          <tr key={id}>
+            <th>{id}</th>
+            <td>{title}</td>
+            <td>{description}</td>
+            <td className="text-center text-nowrap">
+              <FaEdit
+                size={20}
+                type="button"
+                data-bs-toggle="modal"
+                data-bs-target="#edit-modal"
+                className="me-2 text-warning"
+              />
+              <AiFillDelete size={22} type="button" className="text-danger " />
+            </td>
+          </tr>
         </tbody>
       </table>
-
-      <EditTutorial editItem={editItem} getTutorials={getTutorials} />
     </div>
   );
 };
