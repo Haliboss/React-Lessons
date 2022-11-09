586c8957d4aca2cff25df5e1915c187701117678
Haliboss <halibos@gmail.com>
2022-11-09 03:21:29 +0300

    add 'a'

--- styled-intro_starter/src/App.js
+++ styled-intro_starter/src/App.js
@@ -1,16 +1,20 @@
 import { Button, TomatoButton } from "./components/Button.styled";
 import Container from "./components/Container.styled";
 import HeaderText from "./components/HeaderText.styled";
+import StyledLink from "./components/Link.styled";
 
 const App = () => {
   return (
     <>
-      <Container bg='grey'>
-        <HeaderText color='hotpink'>STYLED COMPONENTS</HeaderText>
+      <Container bg="grey">
+        <HeaderText color="hotpink">STYLED COMPONENTS</HeaderText>
         <Button primary>Click</Button>
         <Button>Save</Button>
         <TomatoButton>Send</TomatoButton>
         <TomatoButton primary>Submit</TomatoButton>
+      </Container>
+      <Container>
+        <StyledLink href="https://github.com/Haliboss" target='_blank'>Haliboss Demoss</StyledLink>
       </Container>
     </>
   );
