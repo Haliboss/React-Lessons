0a95a3e6a65772f9e1a75e5921fc6d812bb42c7c
Haliboss <halibos@gmail.com>
2022-11-12 12:39:55 +0300

    added store

--- redux-project/src/features/authSlice.jsx
+++ redux-project/src/features/authSlice.jsx
@@ -8,7 +8,14 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setUser: () => {},
-    clearUser: () => {},
+    setUser: (state, { payload }) => {
+      state.user = payload;
+    },
+    clearUser: (state) => {
+      state.user = null;
+    },
   },
 });
+
+export const {setUser, clearUser} = authSlice.actions;
+export default authSlice.reducer;
