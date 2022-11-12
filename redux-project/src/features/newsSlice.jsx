e7c8946eeef314214e6afc2bac5b899cccf3090a
Haliboss <halibos@gmail.com>
2022-11-12 14:04:11 +0300

    rejected error

--- redux-project/src/features/newsSlice.jsx
+++ redux-project/src/features/newsSlice.jsx
@@ -1,10 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   name: [],
   loading: false,
   error: false,
 };
+
+export const getNews = createAsyncThunk(
+  "getNews",
+  async (thunk, { rejectWithValue }) => {
+    const API_KEY = "9c9d2f8aaa164bce8e499ab00ac40e1";
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+    try {
+      const { data } = await axios(url);
+      return data.articles;
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue("something went wrong...");
+    }
+  }
+);
 
 const newsSlice = createSlice({
   name: "news",
@@ -13,6 +29,20 @@ const newsSlice = createSlice({
     clearNewsList: (state) => {
       state.newsList = [];
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getNews.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getNews.fulfilled, (state, { payload }) => {
+        state.newsList = payload;
+        state.loading = false;
+      })
+      .addCase(getNews.rejected, (state) => {
+        state.loading = false;
+        state.error = true;
+      });
   },
 });
 
