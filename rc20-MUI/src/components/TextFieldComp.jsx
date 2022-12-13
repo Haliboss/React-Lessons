30b8ad9762b2a488c08ee4958a947483f689f78e
Haliboss <halibos@gmail.com>
2022-12-13 16:19:27 +0300

    text field

---
+++ rc20-MUI/src/components/TextFieldComp.jsx
@@ -0,0 +1,41 @@
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+
+const TextFieldComp = () => {
+  const [error, setError] = useState(false);
+
+  return (
+    <div>
+      <Container>
+        <Typography variant="h4" color="error" align="center" mt={4}>
+          TEXT FIELD
+        </Typography>
+        <Box mt={4}>
+          <TextField
+            id="email"
+            label="Email"
+            placeholder="Enter Your Mail"
+            fullWidth
+            error={error}
+            helperText={error && "Incorrect email format"}
+          />
+          <TextField
+            margin="normal"
+            id="password"
+            label="Password"
+            placeholder="Enter Your Password"
+            fullWidth
+            error={error}
+            helperText={error && "Incorrect password format"}
+          />
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default TextFieldComp;
