16315d3bd40caa29850f54b1a3a2a3387f1b7e76
Haliboss <halibos@gmail.com>
2022-12-13 13:05:08 +0300

    add component

--- rc20-MUI/src/components/TypoButtons.jsx
+++ rc20-MUI/src/components/TypoButtons.jsx
@@ -1,5 +1,17 @@
+import Typography from '@mui/material/Typography'
+
 const TypoButtons = () => {
-  return <div>TypoButtons</div>;
+  return (
+    <div>
+      <Typography variant="h4" component="h2" mt={4} align='center' >
+        MUI Typography
+      </Typography>
+
+      <Typography variant="body" m={1} align='center' >
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim debitis nesciunt reprehenderit explicabo dicta tempora molestias. Provident, itaque! Tempora expedita molestiae numquam minima amet alias corporis accusamus deserunt ipsum ipsam.
+      </Typography>
+    </div>
+  );
 };
 
 export default TypoButtons;
